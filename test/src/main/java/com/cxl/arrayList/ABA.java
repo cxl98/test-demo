c218604c30ce1813ae9932320b9194f45819d90b
cxl <1799359457@qq.com>
2020-06-19 18:11:21 +0800

    update

---
+++ test/src/main/java/com/cxl/arrayList/ABA.java
@@ -0,0 +1,23 @@
+package com.cxl.arrayList;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ABA {
+    public static void main(String[] args) {
+        AtomicReference<Integer> atomicReference=new AtomicReference<>();
+        atomicReference.set(100);
+        new Thread(()->{
+            atomicReference.compareAndSet(100,101);
+            atomicReference.compareAndSet(101,100);
+        },"t1").start();
+       new Thread(()->{
+           try {
+               TimeUnit.SECONDS.sleep(4);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+           System.out.println(atomicReference.compareAndSet(100,2020)+"\t"+atomicReference.get());
+       },"t2").start();
+    }
+}
--- test/src/main/java/com/cxl/arrayList/TestHashMap.java
+++ test/src/main/java/com/cxl/arrayList/TestHashMap.java
@@ -1,13 +1,12 @@
 package com.cxl.arrayList;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TestHashMap {
     public static void main(String[] args) {
-        Map<Integer, Integer> map=new HashMap<>();
+        Map<Integer, Integer> map=new ConcurrentHashMap<>();
         map.put(1,2);
         map.put(2,2);
-        System.out.println(1<<30);
     }
 }
