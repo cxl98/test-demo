2055ea8a49b174f173e6c5d33bb129483ab6ec4a
cxl <1799359457@qq.com>
2020-09-11 22:13:54 +0800

    interpreter

--- test/src/main/java/com/cxl/reboot/Reboot.java
+++ test/src/main/java/com/cxl/reboot/Reboot.java
@@ -1,9 +1,12 @@
 package com.cxl.reboot;
 
+import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
-public class Reboot {
-    public static void main(String[] args) {
-
+public class Reboot extends JComponent {
+    public static void main(String[] args) throws IOException {
+        int read = System.in.read();
+        System.out.println(read);
     }
 }
