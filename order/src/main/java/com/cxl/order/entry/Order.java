8cc9c7db819570531c8dd3c85fc5c454def5c1d0
cxl <1799359457@qq.com>
2020-09-19 23:00:54 +0800

    订单表

--- order/src/main/java/com/cxl/order/entry/Order.java
+++ order/src/main/java/com/cxl/order/entry/Order.java
@@ -1,4 +1,13 @@
 package com.cxl.order.entry;
 
-public class Order {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Order implements Serializable {
+
+    private Long id;//订单id
+    private String orderSn;//订单编号
+    private Date createTime;//提交时间
+    private String
+
 }
