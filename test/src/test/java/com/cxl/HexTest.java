f63b185aa5b3f4fb35d799ecf9a78462cb54bfb9
cxl <1799359457@qq.com>
2020-11-21 17:33:35 +0800

    16进制工具

--- test/src/test/java/com/cxl/HexTest.java
+++ test/src/test/java/com/cxl/HexTest.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 
 public class HexTest {
     public static void main(String[] args) {
-        String str="�����J��";
+        String str="e4b88a";
         byte[] bytes = HexUtil.hexStringToBytes(str);
         String s = new String(bytes, StandardCharsets.UTF_8);
         System.out.println(s);
