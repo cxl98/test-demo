c4ccdb65860d41d386197acab3d6d248b6561c38
cxl <1799359457@qq.com>
2021-03-21 08:44:32 +0800

    十大排序算法

--- test/src/main/java/com/cxl/algorithm/Sort.java
+++ test/src/main/java/com/cxl/algorithm/Sort.java
@@ -151,7 +151,7 @@ public class Sort {
     }
 
     private int part(int[] array, int start, int end) {
-        int index = start -1;
+        int index = start - 1;
         for (int i = start; i <= end; i++) {
             if (array[i] <= array[end]) {
                 index++;
@@ -169,13 +169,103 @@ public class Sort {
         array[j] = temp;
     }
 
+
+    int len;
+
+    /**
+     * 堆排序
+     *
+     * @param array
+     * @return
+     */
+    public int[] heapSort(int[] array) {
+        len = array.length;
+        if (len < 1) {
+            return array;
+        }
+        //构建一个最大堆
+        buildMaxHeap(array);
+        //循环将堆首位(最大值)与末位交换，然后再重新调整最大堆
+        while (len > 0) {
+            swap(array, 0, len - 1);
+            len--;
+            adjustMaxHeap(array, 0);
+        }
+        return array;
+    }
+
+    /**
+     * 建立最大堆
+     *
+     * @param array
+     */
+    private void buildMaxHeap(int[] array) {
+        for (int i = (len / 2 - 1); i >= 0; i--) {
+            adjustMaxHeap(array, i);
+        }
+    }
+
+    /**
+     * 调整最大堆
+     *
+     * @param array
+     * @param i
+     */
+    private void adjustMaxHeap(int[] array, int i) {
+        int maxIndex = i;
+        //如果有左子树，且左子树大于父节点，则将最大指针指向左子树
+        if (i * 2 < len && array[i * 2] > array[maxIndex]) {
+            maxIndex = i * 2;
+        }
+        if (i * 2 + 1 < len && array[i * 2 + 1] > array[maxIndex]) {
+            maxIndex = i * 2 + 1;
+        }
+        if (maxIndex != i) {
+            swap(array, maxIndex, i);
+            adjustMaxHeap(array, maxIndex);
+        }
+    }
+
+    public int[] countSort(int[] array) {
+        if (array.length == 0) {
+            return array;
+        }
+        int bits, min = array[0], max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        bits = -min;
+        int[] bucket = new int[max - min + 1];
+        Arrays.fill(bucket, 0);
+        for (int i = 0; i < array.length; i++) {
+            bucket[array[i] + bits]++;
+        }
+        int index = 0, i = 0;
+        while (index < array.length) {
+            if (bucket[i] != 0) {
+                array[index] = i - bits;
+                bucket[i]--;
+                index++;
+            } else {
+                i++;
+            }
+        }
+        return array;
+    }
+
     public static void main(String[] args) {
 
-        int array[] = {2, 3, 4, 8, 5, 1, 1, 21, 4, 12, 43, 4};
+        int array[] = {2, 3, 4, 8, 5, 1, 1, 21, 4, 12};
         Sort sort = new Sort();
-        int[] ints = sort.quitSort(array,0,array.length-1);
+//        int[] ints = sort.quitSort(array, 0, array.length - 1);
+        int[] ints = sort.heapSort(array);
         for (int item : ints) {
-            System.out.println(item);
+            System.out.println(item);  
         }
     }
 }
---
+++ test/src/main/java/com/cxl/algorithm/SumAdd.java
@@ -0,0 +1,66 @@
+package com.cxl.algorithm;
+
+public class SumAdd {
+     static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    static class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode listNode = new ListNode(0);
+            ListNode p = l1, q = l2, cur = listNode;
+            int carry = 0;
+            while (p != null || q != null) {
+                int x = (p != null) ? p.val : 0;
+                int y = (q != null) ? q.val : 0;
+                int sum = carry + x + y;
+                carry = sum / 10;
+                cur.next = new ListNode(sum % 10);
+                cur = cur.next;
+                if (p != null) {
+                    p = p.next;
+                }
+                if (q != null) {
+                    q = q.next;
+                }
+            }
+            if (carry > 0) {
+                cur.next = new ListNode(carry);
+            }
+            return listNode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+//        l1 = [2,4,3], l2 = [5,6,4]
+        ListNode listNode=new ListNode(2);
+        ListNode listNode1=new ListNode(4);
+        ListNode listNode3=new ListNode(3);
+        listNode.next=listNode1;
+        listNode1.next=listNode3;
+        ListNode listNode4=new ListNode(5);
+        ListNode listNode5=new ListNode(6);
+        ListNode listNode6=new ListNode(4);
+        listNode4.next=listNode5;
+        listNode5.next=listNode6;
+        Solution solution=new Solution();
+        ListNode listNode2 = solution.addTwoNumbers(listNode, listNode4);
+        while (listNode2!=null){
+            System.out.println(listNode2.val);
+            listNode2=listNode2.next;
+        }
+    }
+}
