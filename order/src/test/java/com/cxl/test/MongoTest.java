86dde841579e6dec45cb9600a88c114a24980b3e
cxl <1799359457@qq.com>
2021-06-04 09:13:06 +0800

    配置docker远程连接

--- order/src/test/java/com/cxl/test/MongoTest.java
+++ order/src/test/java/com/cxl/test/MongoTest.java
@@ -1,26 +1,26 @@
-package com.cxl.test;
-
-import com.cxl.order.OrderApplication;
-import com.cxl.order.dao.UserRepository;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = OrderApplication.class)
-public class MongoTest {
-    @Autowired
-    private UserRepository userRepository;
-    @Before
-    public void init(){
-        userRepository.deleteAll();
-    }
-    @Test
-    public void userTest(){
-
-    }
-
-}
+//package com.cxl.test;
+//
+//import com.cxl.order.OrderApplication;
+//import com.cxl.order.dao.UserRepository;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = OrderApplication.class)
+//public class MongoTest {
+//    @Autowired
+//    private UserRepository userRepository;
+//    @Before
+//    public void init(){
+//        userRepository.deleteAll();
+//    }
+//    @Test
+//    public void userTest(){
+//
+//    }
+//
+//}
